46b5e02f39477cf989ee34d2474dfb4b4c2b2a25
Jason Wibowo <jwibowo@butlercc.edu>
2020-06-02 22:45:52 -0500

    database connection

--- Program.cs
+++ Program.cs
@@ -6,25 +6,38 @@ using System.Threading.Tasks;
 using System.Data.OleDb;
 using Microsoft.Office.Interop.PowerPoint;
 using Microsoft.Office.Interop.Access;
+using System.Data;
+using DataTable = System.Data.DataTable;
+using System.Data.Common;
+using tutor_testing_v3.TutorDataSetTableAdapters;
+using System.Diagnostics;
 
-namespace tutor_testing_v3
-{
-    class Program
-    {
-        public static OleDbConnection myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\assets\\Tutor.accdb");
+namespace tutor_testing_v3{
+    class Program{
         public static Microsoft.Office.Interop.PowerPoint.Application application = new Microsoft.Office.Interop.PowerPoint.Application();
         public static Presentations ppPresens = application.Presentations;
         public static Presentation objPres = ppPresens.Open(AppDomain.CurrentDomain.BaseDirectory + "\\assets\\better powerpoint test v2.pptm", Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoTrue);
         public static Slides objSlides = objPres.Slides;
         public static SlideShowSettings objSSS = objPres.SlideShowSettings;
-        static void Main(string[] args)
-        {
+        public static TutorDataSet db = new TutorDataSet();
+        public static TutorDataSet.AllTutorsDataTable tutorTable = new TutorDataSet.AllTutorsDataTable();
+        public static TutorDataSet.ScheduleDataTable scheduleTable = new TutorDataSet.ScheduleDataTable();
+        public static TutorDataSet.SubjectDataTable subjectTable = new TutorDataSet.SubjectDataTable();
+
+        static void Main(string[] args) {
             Init();
         }
         static void Init() {
-            myConn.Open();
+
             application.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
             objSSS.Run();
+            AllTutorsTableAdapter tutorTableAdapt = new AllTutorsTableAdapter();
+            tutorTableAdapt.Fill(tutorTable);
+            ScheduleTableAdapter scheduleAdapt = new ScheduleTableAdapter();
+            scheduleAdapt.Fill(scheduleTable);
+            SubjectTableAdapter subjectAdapt = new SubjectTableAdapter();
+            subjectAdapt.Fill(subjectTable);
+
         }
     }
 }
